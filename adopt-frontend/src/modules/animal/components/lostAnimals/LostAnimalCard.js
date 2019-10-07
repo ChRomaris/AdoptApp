2d8f2a25716f200bf60fd2126d6b2ee70d554a19
c.romaris <c.romaris@udc.es>
2019-10-07 18:48:08 +0200

    solucion error commit

---
+++ adopt-frontend/src/modules/animal/components/lostAnimals/LostAnimalCard.js
@@ -0,0 +1,49 @@
+import React,{ Component } from "react";
+import Moment from 'moment';
+import {CardHeader, CardBody, CardTitle, CardText, Button, CardFooter,Card} from 'reactstrap';
+import PreviewImage from '../../../mainList/components/PreviewImage';
+import {FormattedMessage} from 'react-intl'
+
+class LostAnimalCard extends Component{
+    constructor(props){
+        super()
+
+        this.renderGenre = this.renderGenre.bind(this)
+    }
+
+    renderGenre(){
+        if(this.props.animal.genre === "MALE" ){
+           return <FormattedMessage id='form.enum.male'/>  
+        }else if(this.props.animal.genre === "FEMALE"){
+           return <FormattedMessage id='form.enum.female'/>  
+        }       
+    }
+
+
+    render(){
+        
+        return(
+            <Card>
+                <CardHeader>{this.props.animal.name}</CardHeader>
+                <CardBody className="cardBody">
+                    <div className="picture">
+                        <PreviewImage image={this.props.animal.image}/>
+                    </div>
+                    <div className="texts">
+                        <CardTitle><FormattedMessage id='form.label.date'/> : {Moment(this.props.animal.date).format("DD-MM-YY")}</CardTitle>
+                        <CardTitle><FormattedMessage id='form.label.genre'/> : {this.renderGenre()} </CardTitle>
+                        <CardTitle><FormattedMessage id='form.label.breed'/> : {this.props.animal.breed} </CardTitle>
+                        <CardText><FormattedMessage id='form.label.distance'/> : {this.props.animal.distance}</CardText>
+                    </div>  
+                </CardBody>
+                <CardFooter>
+                    <Button>Mas Información</Button>
+                    <Button>Localizar</Button>
+                </CardFooter>
+            
+            </Card>
+        )
+    }
+}
+
+export default LostAnimalCard
---
+++ adopt-frontend/src/modules/animal/components/lostAnimals/LostAnimalsList.js
@@ -0,0 +1,75 @@
+import React,{ Component } from "react";
+import { ToastsContainer, ToastsStore } from 'react-toasts';
+import { Container, Button } from 'reactstrap';
+import LostAnimalCard from './LostAnimalCard';
+
+import '../animalStyles.css'
+
+class LostAnimalsList extends Component{
+    constructor(props){
+        super()
+        this.state = {
+            animalList : []
+        }
+
+        this.renderButtons = this.renderButtons.bind(this)
+    }
+
+    renderButtons(){
+        console.log("maxima" + this.props.maxPage)
+        console.log("actual" + this.props.actualPage)
+        if(this.props.maxPage === this.props.actualPage)
+        {
+            return(
+                <div className ="previousButton">
+                <Button onClick={this.props.previousPage} >
+                    Anterior
+                </Button>
+                </div>
+            )
+        }else if (this.props.actualPage === 0){
+            return(
+                <div className ="previousButton">
+                <Button onClick={this.props.nextPage} >
+                    Siguiente
+                </Button>
+                </div> 
+            )
+        }else{
+            return(
+                <div className = "previousNextButtons">
+                     <div className ="previousButton">
+                        <Button onClick={this.props.previousPage} >
+                            Anterior
+                        </Button>
+                    </div>
+                    <div className ="nextButton">
+                        <Button onClick={this.props.nextPage} >
+                            Siguiente
+                        </Button>
+                    </div> 
+                </div>
+            )
+        }
+    }
+
+    render(){
+        return(
+            <div>
+            <Container>
+                <ToastsContainer store={ToastsStore} />
+                <div>
+                    {this.props.animalList.map(animal => (
+                        <LostAnimalCard key={animal.name} animal={animal}></LostAnimalCard>
+                    ))}
+                </div>
+                {this.renderButtons()}
+            </Container>
+            
+        </div> 
+        )
+    }
+}
+
+export default LostAnimalsList
+
